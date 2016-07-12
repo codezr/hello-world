562e0a511f92e9856891d71cd8b92ecb7d94f3c6
Roger Zetterström <rogerze@gmail.com>
2016-07-12 16:33:46 +0200

    bugfix3

--- Hello-World/Hello-World/Program.cs
+++ Hello-World/Hello-World/Program.cs
@@ -10,7 +10,7 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World V2");
+            System.Console.WriteLine("Hello World V3");
         }
     }
 }
