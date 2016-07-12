da5f580226ad4e56e36830c6df9ff7d7a2f28f00
codezr <rogerze@gmail.com>
2016-07-12 16:25:31 +0200

    Update Program.cs

--- Hello-World/Hello-World/Program.cs
+++ Hello-World/Hello-World/Program.cs
@@ -10,7 +10,7 @@ namespace Hello_World
     {
         static void Main(string[] args)
         {
-            System.Console.WriteLine("Hello World");
+            System.Console.WriteLine("Hello World V2");
         }
     }
 }
